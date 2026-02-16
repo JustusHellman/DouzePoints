896680be3cc4f133e3d26f23ed5c7342b349f45b
JustusHellman <justusmhellman@gmail.com>
2026-02-16 22:35:47 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Douzepoints/',
+  base: '/./',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
   server: {
     port: 3000,
